fcab8a269cfcf67a573438e03510708d7c510b0f
Josh Phang <jphang@sfu.ca>
2023-09-15 16:23:32 -0700

    Beginning flutter tutorial

--- druggle/lib/main.dart
+++ druggle/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:druggle/pages/home.dart';
 import 'package:flutter/material.dart';
 
 //Launch emulator with
@@ -11,46 +12,16 @@ import 'package:flutter/material.dart';
 //https://lhncbc.nlm.nih.gov/RxNav/APIs/index.html
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text('Flutter is Fun!'),
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            const Icon(Icons.backpack),
-            const Icon(Icons.leaderboard),
-            const Icon(Icons.person),
-            FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-            ),
-            Text('$counter'),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
